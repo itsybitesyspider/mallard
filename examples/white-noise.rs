8a5e46472018976441abdf275a105bd8174c18f8
itsybitesyspider <clanehin@gmail.com>
2021-01-05 21:06:30 -0500

    Move white noise generation to texture

--- examples/white-noise.rs
+++ examples/white-noise.rs
@@ -5,7 +5,7 @@ fn main() {
     println!("creating renderer...");
     let renderer = block_on(HeadlessRenderer::new());
     println!("generating canvas...");
-    let canvas = block_on(Canvas::new_from_white_noise(&renderer, (512,512)));
+    let canvas = block_on(TextureRgba8Color::new_from_white_noise(&renderer, (512,512)));
     println!("writing output...");
     block_on(canvas.save(renderer, "white-noise.png"));
 }
